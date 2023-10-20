07b5eb1c2edaf38c6006b86937829af1ab9107e3
Orpheric ALLAGBE <florentallgbe229@gmail.com>
2023-10-20 15:20:01 +0100

    Livewire Component Created

---
+++ app/Livewire/ParentChildren.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ParentChildren extends Component
+{
+    public function render()
+    {
+        return view('livewire.parent-children');
+    }
+}
