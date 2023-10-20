41f6cbc92c5a3d21a7b5e2ce6661d9bc96f3ef95
Orpheric ALLAGBE <florentallgbe229@gmail.com>
2023-10-20 15:32:05 +0100

    define all property in livewire component

--- app/Livewire/ParentChildren.php
+++ app/Livewire/ParentChildren.php
@@ -3,9 +3,25 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\AppleProduct;
+use Illuminate\Support\Collection;
 
 class ParentChildren extends Component
 {
+    public string $customer_name = '';
+
+    public string $customer_email = '';
+
+    public Collection allProducts;
+
+    public array orderProducts = [];
+
+    public function mount(): void {
+        $this->allProducts = AppleProduct::all();
+
+        $this->orderProducts[] = ['product_id' => '', 'quantity' => 1];
+    }
+
     public function render()
     {
         return view('livewire.parent-children');
